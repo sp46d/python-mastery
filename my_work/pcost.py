4af43a06a6834451bb88b8b62cd803009ebdb37f
Sanghyuk Park <sanghyuk.park85@gmail.com>
2024-09-11 11:29:13 -0500

    Modify pcost.py to solve Exercise 1.4

--- my_work/pcost.py
+++ my_work/pcost.py
@@ -1,15 +1,25 @@
 import os
+from pathlib import Path
+
+
+def portfolio_cost(filename: Path):
+    total_sum: float = 0.0
+    with open(filename, "r") as f:
+        for line in f:
+            try:
+                match line.split():
+                    case [_, quantity, price]:
+                        total_sum += int(quantity) * float(price)
+            except ValueError as e:
+                print("Couldn't parse:", line)
+                print("Reason:", e)
+            finally:
+                continue
+
+    return total_sum
 
 
 dirname = os.path.dirname(os.path.dirname(__file__))
-dat_path = os.path.join(dirname, "Data", "portfolio.dat")
+dat_path = Path(os.path.join(dirname, "Data", "portfolio3.dat"))
 
-
-total_sum: float = 0.0
-with open(dat_path, "r") as f:
-    for line in f:
-        match line.split():
-            case [symbol, quantity, price]:
-                total_sum += int(quantity) * float(price)
-
-print(total_sum)
+print(portfolio_cost(dat_path))
